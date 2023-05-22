0664dde873f3b6a8797adf9da180eaed7967c5b6
Alexander Sapko <sapcoreq@gmail.com>
2023-05-22 20:20:30 +0300

    Added tasks 2 and 3

--- 2_files.py
+++ 2_files.py
@@ -1,3 +1,5 @@
+import string
+
 """
 Домашнее задание №2
 
@@ -11,12 +13,26 @@
 5. Сохраните результат в файл referat2.txt
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        full_text = f.read()
+        print(
+            f'Количество символов в тексте: {len(full_text)}.')
+        text_modified = full_text
+        for char in string.punctuation:
+            text_modified = text_modified.replace(char, '')
+        print(
+            f'Количество слов в тексте: {len(text_modified.split())}.')
+        full_text = full_text.replace('.', '!')
+
+    with open('referat2.txt', 'w', encoding='utf-8') as f:
+        f.write(full_text)
+
 
 if __name__ == "__main__":
     main()
